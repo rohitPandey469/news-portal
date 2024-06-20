31cf0bf44553f88e030f751969af56fb3aeb6dc2
Rohit Pandey <rd8614196@gmail.com>
2024-06-20 10:34:11 +0530

    Prepared for viewing the task

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY
+  ? import.meta.env.VITE_NEWS_API_KEY
+  : "c8b3145d5c274d519e48b5eac56dd04e";
 const BASE_URL = "https://newsapi.org/v2";
 
 export const fetchNewsFromApi = (category: string, page: number) => {
